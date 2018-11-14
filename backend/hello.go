80b1686b957f0290a87bb524f1c035ffc4a45c3f
Philipp Fehr <philipp@thefehr.me>
2018-11-14 15:17:34 +0100

    Update hello.go
    Does lint fail?

--- backend/hello.go
+++ backend/hello.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello from da go\n")
+	fmt.Printf( "Hello from da go\n")
 	fmt.Printf("Hoi Philipp\n")
 	fmt.Printf("Ich bin schon fast ein ganzer Server!\n")
 }
 
-func Calc(a int) (result int) {
+func Calc(a int) (result int) 
+{
 	return a + 2
 }
