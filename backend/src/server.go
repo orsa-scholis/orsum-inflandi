8d964442405e152cb37ab9f7decf110d697a2f66
Lukas Bischof <polinderis@gmail.com>
2018-11-15 20:36:36 +0100

    Introduce editor config

--- backend/src/server.go
+++ backend/src/server.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"strings"
+  "bufio"
+  "fmt"
+  "net"
+  "strings"
 )
 
 func startServer() {
-	fmt.Println("Launching server...")
+  fmt.Println("Launching server...")
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":4560")
+  // listen on all interfaces
+  ln, _ := net.Listen("tcp", ":4560")
 
-	// accept connection on port
-	conn, _ := ln.Accept()
+  // accept connection on port
+  conn, _ := ln.Accept()
 
-	// run loop forever (or until ctrl-c)
-	for {
-		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
-	}
+  // run loop forever (or until ctrl-c)
+  for {
+    // will listen for message to process ending in newline (\n)
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    // output message received
+    fmt.Print("Message Received:", string(message))
+    // sample process for string received
+    newmessage := strings.ToUpper(message)
+    // send new string back to client
+    conn.Write([]byte(newmessage + "\n"))
+  }
 }
