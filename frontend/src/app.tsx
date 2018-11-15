530ce64747a376c9dd73d009bc5d80f9754d8e9a
Philipp Fehr <philipp@thefehr.me>
2018-11-15 21:51:31 +0100

    Updating react and adding material ui

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import { SnackbarProvider } from 'notistack'
 
 export class App extends React.Component<undefined, undefined> {
   render() {
     return (
-      <div>
-        <h2>Welcome to React with Typescript!</h2>
-      </div>
+      <SnackbarProvider maxSnack={5}>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </SnackbarProvider>
     );
   }
 }
