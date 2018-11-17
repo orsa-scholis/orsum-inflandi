78672072688ed135fd1344630daee5c0113b1019
Lukas Bischof <polinderis@gmail.com>
2018-11-17 13:17:05 +0100

    Fixing linting

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
-import { SnackbarProvider } from 'notistack'
+import { SnackbarProvider } from 'notistack';
 
 export class App extends React.Component<undefined, undefined> {
   render() {
     return (
       <SnackbarProvider maxSnack={5}>
-        <Button variant="contained" color="primary">
+        <Button variant='contained' color='primary'>
           Hello World
         </Button>
       </SnackbarProvider>
