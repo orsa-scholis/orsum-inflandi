4f1dd688df218e7e0c8951819aab0efcf88fb2ee
Philipp Fehr <philipp@thefehr.me>
2018-11-19 14:45:36 +0100

    Listens to Sigint

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "github.com/orsa-scholis/orsum-inflandi-II/backend/server"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/orsa-scholis/orsum-inflandi-II/backend/server"
 )
 
 func main() {
-  server.Start()
-  fmt.Println("Testing")
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanUp()
+	}()
+
+	server.Start()
+}
+
+func cleanUp() {
+	err := server.CleanUp()
+	if nil != err {
+		os.Exit(1)
+	}
+
+	fmt.Println("Shutdown gracefuly")
+	os.Exit(0)
 }
