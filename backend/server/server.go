a08577695125d16fd1cb957b48eb1b98287c3cc3
Philipp Fehr <philipp@thefehr.me>
2018-11-20 13:30:08 +0100

    [WIP] added static legacy publicKey

--- backend/server/server.go
+++ backend/server/server.go
@@ -2,6 +2,11 @@ package server
 
 import (
 	"bufio"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/asn1"
+	b64 "encoding/base64"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -13,13 +18,27 @@ type Client struct {
 	Conn net.Conn
 }
 
+type ServerMessage struct {
+	domain      string
+	command     string
+	param       string
+	attachement string
+}
+
 var clients []Client
+var ln net.Listener
+var legacy bool
 
 func Start() {
+	flag.BoolVar(&legacy, "legacy", false, "should run in legacy mode?")
+
+	flag.Parse()
+
 	clients := make([]Client, 0)
 	fmt.Println("Launching server...")
 
-	ln, err := net.Listen("tcp", ":4560")
+	var err error
+	ln, err = net.Listen("tcp", ":4560")
 	if nil != err {
 		fmt.Fprint(os.Stderr, "can't create TCP socket: ", err)
 		os.Exit(1)
@@ -33,10 +52,10 @@ func Start() {
 		if nil != err {
 			fmt.Fprint(os.Stderr, "can't accept connection: ", err)
 		} else {
-			newClient := Client {
-			  Name: fmt.Sprintf("Client Nr. %v", len(clients)),
-			  Conn: conn,
-      }
+			newClient := Client{
+				Name: fmt.Sprintf("Client Nr. %v", len(clients)),
+				Conn: conn,
+			}
 			clients = append(clients, newClient)
 			go handleConnection(newClient)
 		}
@@ -44,15 +63,55 @@ func Start() {
 }
 
 func handleConnection(client Client) {
-  message, _ := bufio.NewReader(client.Conn).ReadString('\n')
+	reader := rand.Reader
+	bitSize := 2048
+
+	key, keyErr := rsa.GenerateKey(reader, bitSize)
+	publicKey := key.PublicKey
+
+	if keyErr != nil {
+		fmt.Println(keyErr)
+	}
+
+	var conn = client.Conn
+
+	message, _ := bufio.NewReader(client.Conn).ReadString('\n')
+
+	// TODO: Implement better check func
+	messageParts := strings.Split(message, ":")
+	if len(messageParts) != 3 {
+		fmt.Println("First message too short")
+	}
+	if (messageParts[0]) != "connection" {
+		fmt.Println("First message domain wrong")
+	}
+	if (messageParts[1]) != "connect" {
+		fmt.Println("First message action wrong")
+	}
+	if (len(messageParts[2])) < 4 {
+		fmt.Println("First message param too short")
+	}
+
 	fmt.Print("Message Received:", string(message))
 
-	newMessage := strings.ToUpper(message)
-	_, err := client.Conn.Write([]byte(newMessage + "\n"))
-
+	if legacy {
+		message = fmt.Sprintf("success:accepted:rO0ABXNyABRqYXZhLnNlY3VyaXR5LktleVJlcL35T7OImqVDAgAETAAJYWxnb3JpdGhtdAASTGphdmEvbGFuZy9TdHJpbmc7WwAHZW5jb2RlZHQAAltCTAAGZm9ybWF0cQB+AAFMAAR0eXBldAAbTGphdmEvc2VjdXJpdHkvS2V5UmVwJFR5cGU7eHB0AANSU0F1cgACW0Ks8xf4BghU4AIAAHhwAAAAojCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAogfCMH/o4tfQHdN6ABXw8w02kbUJE4XQSfYq43kRu9BbRZOF4bCCBrL4G4lqMGmcuJiP5wSIpkjFG5C+BnZx1OLghC/kOThyhhI+cfDL2hgr/It8ELrdw3CqhugGfRzZJQD9ZkjdvceD2Wts00cyPfpJTAG+KVFeEJcg1knFN5ECAwEAAXQABVguNTA5fnIAGWphdmEuc2VjdXJpdHkuS2V5UmVwJFR5cGUAAAAAAAAAABIAAHhyAA5qYXZhLmxhbmcuRW51bQAAAAAAAAAAEgAAeHB0AAZQVUJMSUM=")
+	} else {
+		// TODO: Implement correct key exchange
+		publicBytes, err := asn1.Marshal(publicKey)
+		if nil != err {
+			panic(err)
+		}
+		var publicAs64 = b64.StdEncoding.EncodeToString(publicBytes)
+		message = fmt.Sprintf("success:accepted:%v\n", publicAs64)
+	}
+	_, err := conn.Write([]byte(message))
 	if nil != err {
 		fmt.Println("No writy writy")
+	} else {
+		fmt.Printf("Message Sent: '%v'\n", message)
 	}
+
 }
 
 func CleanUp() error {
@@ -63,11 +122,15 @@ func CleanUp() error {
 		if nil != err {
 			return err
 		}
-
 		err = conn.Close()
 		if nil != err {
 			return err
 		}
+	}
+
+	err := ln.Close()
+	if nil != err {
+		return err
 	}
 
 	return nil
