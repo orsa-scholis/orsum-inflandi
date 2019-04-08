626c76382fa4eea802bc06ea198b1b823c18767f
Philipp Fehr <philipp@thefehr.me>
2019-04-08 09:06:35 +0200

    Fixed MUI Typography deprecation warning

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { SnackbarProvider } from 'notistack';
 import LobbyScreen from './containers/LobbyScreen/LobbyScreen';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 export class App extends React.Component {
   render() {
     return (
-      <SnackbarProvider maxSnack={5}>
-        <LobbyScreen />
-      </SnackbarProvider>
+      <MuiThemeProvider theme={theme}>
+        <SnackbarProvider maxSnack={5}>
+          <LobbyScreen />
+        </SnackbarProvider>
+      </MuiThemeProvider>
     );
   }
 }
+
