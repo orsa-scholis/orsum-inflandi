af9ee9d305b4e33faad6cbee30bf1eb3cc31c8d2
Lukas Bischof <polinderis@gmail.com>
2019-07-02 23:33:58 +0200

    Introduce types

--- cli/commands/base.rb
+++ cli/commands/base.rb
@@ -1,14 +1,21 @@
+# typed: true
 # frozen_string_literal: true
+
+require 'sorbet-runtime'
 
 module OrsumInflandi
   module Commands
     class Base
+      extend T::Sig
+
+      sig { params(options: T::Hash[Symbol, T.untyped]).void }
       def initialize(options)
         @options = options
       end
 
       protected
 
+      sig { params(threads: T::Array[Thread]).returns(T.noreturn) }
       def kill_threads(threads)
         puts "\n"
         Logger.new('Received KILL SIGNAL, shutting down...').info_log
@@ -16,6 +23,7 @@ module OrsumInflandi
         exit
       end
 
+      sig { params(block: T.nilable(T.proc.params(arg0: Integer).returns(BasicObject))).void }
       def trap_kill_signal(&block)
         %w[INT TERM].each do |signal|
           Signal.trap(signal, &block) if block_given?
